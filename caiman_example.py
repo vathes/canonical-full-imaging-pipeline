7c8fd4b9376f835ccb0969d08e29a0d976e947c1
Kabilar Gunalan <kabilar@vathes.com>
2020-12-07 12:05:57 -0600

    Update caiman_example.py

--- caiman_example.py
+++ caiman_example.py
@@ -1,45 +1,32 @@
-import cv2
-import glob
-import logging
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-
-try:
-    cv2.setNumThreads(0)
-except():
-    pass
-
-try:
-    if __IPYTHON__:
-        # this is used for debugging purposes only. allows to reload classes when changed
-        get_ipython().magic('load_ext autoreload')
-        get_ipython().magic('autoreload 2')
-except NameError:
-    pass
-
-import caiman as cm
-from caiman.motion_correction import MotionCorrect
-from caiman.source_extraction.cnmf import cnmf as cnmf
-from caiman.source_extraction.cnmf import params as params
-from caiman.utils.visualization import plot_contours, nb_view_patches, nb_plot_contour
+#%% 
+import scanreader
+from img_loaders import caiman_loader, parse_scanimage_header
 
 #%% Declare file paths
-caiman_loader_dir   = '~/canonical-imaging/img_loaders'
 session_dir         = ''
-input_files         = [session_dir+'/.tif']
+input_file_combined = session_dir+'/.tif'
+input_file_split    = [session_dir+'/.tif']
 output_file         = session_dir+'/caiman/analysis_results.hdf5'
 
-#%%
-import sys
-sys.path.insert(1, caiman_loader_dir)
-import caiman_loader
+#%% Reshape 5 dimensional ScanImage TIFF file and save new TIFF file for each channel
+caiman_loader.split_tiff_channels(input_file_combined, session_dir)
+
+#%% Split TIFF files each with a certain number of frames
+caiman_loader.split_tiff_frames(input_file_combined)
 
 #%% Define parameters
+# Get frame rate from ScanImage header
+scan                = scanreader.read_scan(input_file_combined)
+header              = parse_scanimage_header(scan)
+frame_rate          = int(header['SI_hRoiManager_scanFrameRate'])
+
 # Dataset dependent parameters
-fr                  = 40            # imaging rate [frames per second]
+fr                  = frame_rate    # imaging rate [frames per second]
 decay_time          = 0.4           # length of a typical transient [s]
 is3D                = False
+dxy                 = (2., 2.)      # spatial resolution in x and y [um per pixel]
+max_shift_um        = (12., 12.)    # maximum shift [um]
+patch_motion_um     = (100., 100.)  # patch size for non-rigid correction [um]
 
 # Motion correction parameters
 if is3D:
@@ -75,12 +62,16 @@ min_SNR             = 2.0           # signal to noise ratio for accepting a comp
 rval_thr            = 0.85          # space correlation threshold for accepting a component
 cnn_thr             = 0.99          # threshold for CNN based classifier
 cnn_lowest          = 0.1           # neurons with cnn probability lower than this value are rejected
-use_cnn             = True
+if is3D:
+    use_cnn         = False
+else:
+    use_cnn         = True
 
-#%% Create parameters dictionary
-opts_dict = {'fnames': input_files,
+# Create parameters dictionary
+opts_dict = {'fnames': input_file_split,
             'fr': fr,
             'decay_time': decay_time,
+            'dxy': dxy,
             'strides': strides,
             'overlaps': overlaps,
             'max_shifts': max_shifts,
@@ -105,28 +96,51 @@ opts_dict = {'fnames': input_files,
             'min_cnn_thr': cnn_thr,
             'cnn_lowest': cnn_lowest}
 
-#%% Convert TIFF file to multipage format
-# https://caiman.readthedocs.io/en/master/On_file_types_and_sizes.html
-def convert_tiff(input_file):
-    m			=	cm.load(input_file)#,is3D=True)		# Load file
-    T			=	m.shape[0]							# Total number of frames for the file
-    L			=	1000								# Individual file length
-    fileparts	=	input_file.split('.')
+#%% CaImAn analysis example function
+def caiman_analysis(opts_dict, output_file):
+    """
+    Example CaImAn workflow
+    """
 
-    for t in np.arange(0,T,L):
-        sys.stdout.write('\rFile length: %d, Total frames: %d, Current frame: %d' % (L,T,t))
-        sys.stdout.flush()
-        m[t:t+L].save((".").join(fileparts[:-1]) + '_' + str(t//L) + '.' + fileparts[-1])
+    import cv2
+    import glob
+    import logging
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import os
 
-#%% CaImAn analysis example workflow
-def execute_caiman_analysis(input_files, output_file, opts_dict):
+    try:
+        cv2.setNumThreads(0)
+    except():
+        pass
+
+    try:
+        if __IPYTHON__:
+            # this is used for debugging purposes only. allows to reload classes when changed
+            get_ipython().magic('load_ext autoreload')
+            get_ipython().magic('autoreload 2')
+    except NameError:
+        pass
+
+    import caiman as cm
+    from caiman.motion_correction import MotionCorrect
+    from caiman.source_extraction.cnmf import cnmf as cnmf
+    from caiman.source_extraction.cnmf import params as params
+    from caiman.utils.visualization import plot_contours, nb_view_patches, nb_plot_contour
+    from caiman.summary_images import local_correlations_movie_offline
+
     #%%
     logging.basicConfig(format = "%(relativeCreated)12d [%(filename)s:%(funcName)20s():%(lineno)s] [%(process)d] %(message)s",level = logging.WARNING)
 
     #%% Display original movie
-    m_orig = cm.load_movie_chain(input_files)
+    m_orig = cm.load_movie_chain(opts_dict['fnames'], is3D=opts_dict['is3D'])
+    
+    if opts_dict['is3D']:
+        z_layer = 0
+        m_orig = m_orig[:,:,:,z_layer]
+
     ds_ratio = 0.2
-    m_orig.resize(1, 1, ds_ratio).play(q_max=99.5, fr=fr, magnification=2)
+    m_orig.resize(1, 1, ds_ratio).play(q_max=99.5, fr=opts_dict['fr'], magnification=2)
 
     #%% Create parameters object
     opts = params.CNMFParams(params_dict=opts_dict)
@@ -136,23 +150,40 @@ def execute_caiman_analysis(input_files, output_file, opts_dict):
         cm.stop_server(dview=dview)
     c, dview, n_processes = cm.cluster.setup_cluster(backend='local', n_processes=None, single_thread=False)
 
-    #%% Create a motion correction object with the parameters specified
-    mc = MotionCorrect(input_files, dview=dview, **opts.get_group('motion'))
+    if opts_dict['is3D']:
+        base_name = os.path.splitext(os.path.basename(opts_dict['fnames'][0]))[0]
+        fname = cm.save_memmap(opts_dict['fnames'], base_name=base_name, is_3D=opts_dict['is3D'], order='C')
+    else:
+        fname = opts_dict['fnames']
+
+    #%% Create a motion correction object with parameters specified
+    mc = MotionCorrect(fname, dview=dview, **opts.get_group('motion'))
 
     #%% Run piecewise-rigid motion correction
     mc.motion_correct(save_movie=True)
 
-    #%% Load motion corrected file
-    m_els = cm.load(mc.fname_tot_els)
-    border_to_0 = 0 if mc.border_nan is 'copy' else mc.border_to_0
-
     #%% Display motion corrected movie
-    m_orig = cm.load_movie_chain(input_files)
+    m_orig = cm.load_movie_chain(opts_dict['fnames'], is3D=opts_dict['is3D'])
+    m_els = cm.load(mc.fname_tot_els, is3D=opts_dict['is3D'])
+
+    if opts_dict['is3D']:
+        z_layer = 0
+        m_orig = m_orig[:,:,:,z_layer]
+        m_els = m_els[:,:,:,z_layer]
+
     ds_ratio = 0.2
-    cm.concatenate([m_orig.resize(1, 1, ds_ratio) - mc.min_mov*mc.nonneg_movie, m_els.resize(1, 1, ds_ratio)], axis=2).play(fr=fr, gain=1, magnification=2, offset=0)
+    cm.concatenate([m_orig.resize(1, 1, ds_ratio) - mc.min_mov*mc.nonneg_movie, m_els.resize(1, 1, ds_ratio)], axis=2).play(fr=opts_dict['fr'], gain=1, q_max=99.5, magnification=2, offset=0)
 
     #%% Memory mapping in order 'C'
-    fname_new = cm.save_memmap(mc.mmap_file, base_name='memmap_', order='C', border_to_0=border_to_0, dview=dview)
+    base_name_mc = os.path.splitext(os.path.basename(mc.fname_tot_els[0]))[0]
+    if opts_dict['pw_rigid']:
+        base_name_mc = base_name_mc + '_els_'
+    else:
+        base_name_mc = base_name_mc + '_rig_'
+    
+    border_to_0 = 0 if mc.border_nan is 'copy' else mc.border_to_0
+    
+    fname_new = cm.save_memmap(mc.mmap_file, base_name=base_name_mc, order='C', border_to_0=border_to_0, dview=dview)
 
     # Load file (format: T x X x Y)
     Yr, dims, T = cm.load_memmap(fname_new)
@@ -163,13 +194,14 @@ def execute_caiman_analysis(input_files, output_file, opts_dict):
     c, dview, n_processes = cm.cluster.setup_cluster(backend='local', n_processes=None, single_thread=False)
 
     #%% Run CNMF
-    cnm = cnmf.CNMF(n_processes, params=opts)#, dview=dview)
+    cnm = cnmf.CNMF(n_processes, params=opts, dview=dview)
     cnm = cnm.fit(images)
 
     #%% Plot all components
-    Cn = cm.local_correlations(images.transpose(1,2,0))
-    Cn[np.isnan(Cn)] = 0
-    cnm.estimates.plot_contours_nb(img=Cn)
+    if not opts_dict['is3D']:
+        Cn = cm.local_correlations(images.transpose(1,2,0))
+        Cn[np.isnan(Cn)] = 0
+        cnm.estimates.plot_contours_nb(img=Cn)
 
     #%% Re-run seeded CNMF on accepted patches to refine and perform deconvolution 
     cnm2 = cnm #cnm.refit(images, dview=dview)
@@ -177,28 +209,29 @@ def execute_caiman_analysis(input_files, output_file, opts_dict):
     #%% Evaluate components
     cnm2.estimates.evaluate_components(images, cnm2.params, dview=dview)
 
-    #%% Plot all components
-    cnm2.estimates.plot_contours_nb(img=Cn, idx=cnm2.estimates.idx_components)
-
-    #%% Plot accepted components
-    cnm2.estimates.nb_view_components(img=Cn, idx=cnm2.estimates.idx_components)
-
-    #%% Plot rejected components
-    if len(cnm2.estimates.idx_components_bad) > 0:
-        cnm2.estimates.nb_view_components(img=Cn, idx=cnm2.estimates.idx_components_bad)
-    else:
-        print("No components were rejected.")
-
     #%% Extract DF/F
     cnm2.estimates.detrend_df_f(quantileMin=8, frames_window=250)
 
     #%% Select high quality components
     cnm2.estimates.select_components(use_object=True)
 
-    #%% Plot final results
-    cnm2.estimates.nb_view_components(img=Cn, denoised_color='red')
+    #%% Plot all components
+    if not opts_dict['is3D']:
+        cnm2.estimates.plot_contours(img=Cn, idx=cnm2.estimates.idx_components)
+
+    #%% Plot accepted component traces
+    if opts_dict['is3D']:
+        cnm2.estimates.nb_view_components_3d(image_type='max', dims=dims, axis=2)
+    else:
+        cnm2.estimates.nb_view_components(images, img=Cn, idx=cnm2.estimates.idx_components)
+
+    #%% Plot rejected component traces
+    if not opts_dict['is3D'] and len(cnm2.estimates.idx_components_bad) > 0:
+        cnm2.estimates.view_components(images, img=Cn, idx=cnm2.estimates.idx_components_bad)
 
     #%% Save results
+    if not opts_dict['is3D']:
+        cnm2.estimates.Cn = Cn
     cnm2.save(output_file)
 
     #%% Stop cluster and clean up log files
@@ -208,17 +241,16 @@ def execute_caiman_analysis(input_files, output_file, opts_dict):
         os.remove(log_file)
 
     #%% Display final movie
-    cnm2.estimates.play_movie(images, q_max=99.9, gain_res=2, magnification=2, bpx=border_to_0, include_bck=False)
+    if not opts_dict['is3D']:
+        cnm2.estimates.play_movie(images, q_max=99.9, gain_res=2, magnification=2, bpx=border_to_0, include_bck=False)
 
-    #%%
-    denoised = cm.movie(cnm2.estimates.A.dot(cnm2.estimates.C) + \
+    #%% Reconstruct denoised movie
+    if not opts_dict['is3D']:
+        denoised = cm.movie(cnm2.estimates.A.dot(cnm2.estimates.C) + \
                         cnm2.estimates.b.dot(cnm2.estimates.f)).reshape(dims + (-1,), order='F').transpose([2, 0, 1])
 
     #%% Save motion correction shifts and summary images
-    caiman_loader.save_mc(mc=mc, caiman_fp=output_file)
-
-#%% Reshape 5 dimensional ScanImage TIFF files
-caiman_loader.process_scanimage_tiff(input_files, session_dir)
+    save_mc(mc=mc, caiman_fp=output_file, is3D=opts_dict['is3D'])
 
 #%% Run CaImAn analysis
-execute_caiman_analysis(input_files, output_file, opts_dict)
+caiman_analysis(opts_dict, output_file)
